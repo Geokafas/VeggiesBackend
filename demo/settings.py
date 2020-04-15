700474157ac23406cea0cf81f0378fc5277009b2
Geokafas <kafaskafaskafas@yahoo.gr>
2020-04-15 19:45:52 +0300

    trexei me debug true localy alla oxi sto heroku. Episeis h localpostgres exei dedomena enw h heroku oxi

--- demo/settings.py
+++ demo/settings.py
@@ -1,12 +1,12 @@
 import os
 import dj_database_url
 
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'production')
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = False # mono gia to developement OTAN tha to kanw deploy tha to svhse
+DEBUG = True # mono gia to developement OTAN tha to kanw deploy tha to svhse
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 ALLOWED_HOSTS = ['veggiestreet.herokuapp.com','127.0.0.1']
