90245d458aff37f6b6ef8521ac4f32e33bcd2f75
Geo kafas <kafaskafaskafas@yahoo.gr>
2020-04-15 18:29:14 +0300

    settings

--- demo/settings.py
+++ demo/settings.py
@@ -5,8 +5,8 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = False # mono gia to developement OTAN tha to kanw deploy tha to svhse
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 ALLOWED_HOSTS = ['veggiestreet.herokuapp.com','127.0.0.1']
