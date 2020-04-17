16131c28173cf8aeda1667722a5e89a6ae08f670
Geokafas <kafaskafaskafas@yahoo.gr>
2020-04-17 14:59:43 +0300

    .

--- demo/settings.py
+++ demo/settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = True # mono gia to developement OTAN tha to kanw deploy tha to svhse
+DEBUG = False # mono gia to developement OTAN tha to kanw deploy tha to svhse
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 #SECRET_KEY = os.environ.get('SECRET_KEY')
